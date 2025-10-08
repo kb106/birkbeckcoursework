20b5533aeed6a6a5f912a2b0754db9ed436aaf03
kb106 <154918742+kb106@users.noreply.github.com>
2025-10-08 18:56:28 +0100

    Update formfunctions.js
    Added invoke WTR rankings function to display JSON output

--- formfunctions.js
+++ formfunctions.js
@@ -1,1 +1,14 @@
-
+function invokeWTR_rankings(){
+var rows        = 3
+var tabletennis = "<table class='divborder'>"
+var count       = 0
+  while(count < numrows){
+    tabletennis += `<tr>
+    <td>${count}</td>
+    <td>${count*count}</td>
+    </tr>`;
+    count++;
+  }
+  document.getElementById('test').innerHTML =
+  `${tabletennis}</table>`;
+}
