0a336eba206e96a3f691bc0441b9f87598a6c262
kb106 <154918742+kb106@users.noreply.github.com>
2025-10-22 18:42:37 +0100

    Update formfunctions.js added fetch and promise to return data to a target DIV for mens tennis results

--- formfunctions.js
+++ formfunctions.js
@@ -12,3 +12,20 @@ var count       = 0
   document.getElementById('test').innerHTML =
   `${tabletennis}</table>`;
 }
+
+function invoke_loadmensresults(){
+  //fetch with a promise
+  fetch('men-grand-slam-winners.json')
+    .then((response) => response.json())
+    .then((data) => {
+      //set vars
+      let output = "<ul>";
+      for(const player of data.winner){
+          //iterate and MUST use firstChild for multiple entries where we need 
+          //to grab the first node
+          output += `<li>${player.winner} : &nbsp;&nbsp;${player.winner.runner-up}</li>` ;
+      }        //return all nodes in a DIV using ID as target
+      document.getElementById("results").innerHTML = `${output}</ul>`;
+      });
+}
+
